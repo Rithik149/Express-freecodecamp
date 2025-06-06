48523e3df5cb81d485f055a93ecb7d0ab360f352
Rithik Puthiyedath <60679530+Rithik149@users.noreply.github.com>
2025-06-06 22:41:47 +0530

    I added .env file

--- myApp.js
+++ myApp.js
@@ -1,12 +1,23 @@
+require('dotenv').config(); // ✅ MUST be at the top
+
 let express = require("express");
 let app = express();
-console.log("Hello World")
 
+console.log("Hello World");
 
-app.get("/",function(req,res){
-  res.send("Hello Express")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", function (req, res) {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+console.log("MESSAGE_STYLE =", process.env.MESSAGE_STYLE);
 module.exports = app;
